4c8ec701c775b81f020fb86e3d71789d609b1a5d
Simon Chang <simonychang.hutlab@gmail.com>
2016-03-17 11:22:14 -0400

    fix: corrected add(), commit(), commits(), and validate() to pass tests

--- anpan/git.py
+++ anpan/git.py
@@ -1,6 +1,12 @@
 import os
+import re
+import collections
 
-from .util import sh, cd, touch
+from .util import sh
+from .util import cd
+from .util import touch
+
+Commit = collections.namedtuple('gitoutput',['id','author','date','msg'])
 
 class Repository(object):
 
@@ -24,8 +30,10 @@ class Repository(object):
         
         if not os.path.isdir(self.path):
             raise IOError("Path does not exist: "+self.path)
+        else:
+            return True
 
-        
+
     def sh(self, *args, **kwargs):
         """Wrapper for shell commands. ``sh`` changes directory to the repo
         path, executes the shell command, and changes directory pack
@@ -42,9 +50,62 @@ class Repository(object):
             cd(here)
         return ret
 
-    def add(self,fname = [])
-        pass
+    def add(self,fname = []):
+        """This method takes the list fname, assumes that the
+        filenames are valid, and executes 'git add'
+        on these files."""
 
-    def commit(self,fname = [], msg='msg', author='author')
-        pass
+        try:
+            self.sh(['git', 'add'] + fname)
+        except IOError:
+            raise ValueError('Invalid file names in ' + str(fname))
+
+    def commit(self,fname = [], msg='msg', author='author'):
+        """This method takes the list fname and executes 'git add'
+        and 'git commit' on the files, using the arguments listed
+        here.  The method then calls 'git log' and searches for
+        the comment, and returns the partial hash from the most
+        recent commit that matches the msg argument.""" 
+
+        authorstr="'" + (author) + "'"
+        msgstr="'" + (msg) + "'"
+        prettyformat="'"+'%h|%an <%ae>|%ai|%s'+"'"
+        commitcmd = ['git','commit', '--message='+msgstr, 
+            '--author='+authorstr ]
+        gitlogcmd = ['git','log','--pretty=format:'+prettyformat, 
+            '--grep='+msgstr, '--max-count=1']
+
+        self.add(fname)
+        self.sh(commitcmd)
+        gitlog=str(self.sh(gitlogcmd))
+        loglist = gitlog.split('|')
+        authorname=(loglist[1].split('<')[0])
+        msgplain=(''.join(loglist[3])).strip()
+
+        if (authorname in gitlog) and (msgplain in gitlog):
+            ci_hash=loglist[0]
+            ci_hash=re.sub(r'[^abcdef0-9]', '', ci_hash) 
+        else:
+            ci_hash="BADHASH"
+
+        return ci_hash 
+
+    def commits(self):
+        """This method returns a generator that includes
+        "id", "author", "date", and "msg" as parts of a
+        named tuple."""
+
+        prettyformat="'"+'%H|%an <%ae>|%ai|%s'+"'"
+        gitlogcmd = ['git','log','--pretty=format:'+ prettyformat]
+        gitlog = self.sh(gitlogcmd)
+        gitlogstr=str(gitlog)
+
+        gitlogstr=gitlogstr.replace('"','').replace("'","").replace('(','') \
+                  .replace(', )','').replace('\\n','|')
+        gitloglist=gitlogstr.split('|')
+
+        for i in xrange (0,len(gitloglist),4):
+            commit=Commit(gitloglist[i],gitloglist[i+1],gitloglist[i+2],
+                   gitloglist[i+3])
+            yield commit
 
